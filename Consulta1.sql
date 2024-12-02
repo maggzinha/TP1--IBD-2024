08dc65ca02cb2df3571b2e0950a4ac156c479171
holandaRay <93339014+maggzinha@users.noreply.github.com>
2024-12-02 04:05:30 -0400

    Consulta1.sql

---
+++ Consulta1.sql
@@ -0,0 +1,7 @@
+'''
+Consultas a serem implementadas: 
+1 - Consulta de perfil do usuário: Recuperar informações do perfil de um usuário específico, incluindo nome, foto, biografia, etc.
+'''
+
+USE connectme;
+SELECT * FROM usuario ORDER BY email;
