3bc7ac69ab44e2b821093e0433fb6ca930dd01eb
holandaRay <93339014+maggzinha@users.noreply.github.com>
2024-12-02 18:03:30 -0400

    Consulta2.sql

--- Consulta2.sql
+++ Consulta2.sql
@@ -2,10 +2,11 @@
 Consultas a serem implementadas: 
 2 - Consulta de conexões de um usuário: Obter a lista de amigos de um determinado usuário. 
 '''
+USE connectme;
 SELECT 
     ipu.nome,
     c.email_conexao AS 'lista de amigos'
 FROM informacoes_perfil_usuario AS ipu
 JOIN usuario AS u ON ipu.id = u.id_perfil_usuario
-JOIN conecta AS c ON c.email_usuario = u.email  -- Corrigido: 'amil_usuario' para 'email_usuario'
-WHERE ipu.id = 1;
+JOIN conecta AS c ON c.email_usuario = u.email  
+WHERE ipu.id = 2;
