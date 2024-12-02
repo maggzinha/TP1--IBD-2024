6fd69cc508b85ef7753e08e9f8f34e787e70bf3b
holandaRay <93339014+maggzinha@users.noreply.github.com>
2024-12-02 04:39:15 -0400

    Consulta6.sql

---
+++ Consulta6.sql
@@ -0,0 +1,6 @@
+'''
+Consultas a serem implementadas: 
+6 - Consulta de busca de usuários: Dado uma string como entrada, buscar os usuários cujos nomes contenham a string fornecida. 
+'''
+SELECT * FROM perfil_usuario 
+WHERE nome LIKE '%moreira%';
