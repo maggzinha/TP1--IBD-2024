1f6e72956acd3f30c9fbd89d76a51b8153c95f68
holandaRay <93339014+maggzinha@users.noreply.github.com>
2024-12-02 10:55:22 -0400

    Consulta3.sql

--- Consulta3.sql
+++ Consulta3.sql
@@ -1,11 +1,13 @@
 '''Consultas a serem implementadas: 
 3 - Consulta de postagens de um usuário: Recuperar todas as postagens feitas por um usuário específico, ordenadas por data de publicação (as mais recentes primeiro). 
 '''
+USE connectme;
 SELECT 
-	tipo, 
-    arquivo, 
-    texto, 
+    email,
+    tipo,
+    arquivo,
+    texto,  
     data_hora
-FROM Postagem
-WHERE email = 'viananina@exemple.org'
+FROM postagem
+WHERE email = 'amanda48@example.org'
 ORDER BY data_hora;
